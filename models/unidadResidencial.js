b52475ffde965fa4972c4d4c26e752ba9f7aa996
srodriguez20 <s.rodriguez20@uniandes.edu.co>
2018-03-23 17:20:13 -0500

    Correccion error en unidadResidencial.js

--- models/unidadResidencial.js
+++ models/unidadResidencial.js
@@ -1,13 +1,29 @@
 // app/models/unidadResidencial.js
 
-var inmueble = require('./inmuebles.js')
+
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var unidadResidencial   = new Schema({
+var cerraduraSchema   = new Schema({
     name: String,
     codigo: String,
-    inmueble: inmueble,
+    tipo:String
+});
+var hubSchema   = new Schema({
+    name: String,
+    codigo: String,
+    cerradura: [cerraduraSchema]
+});
+var inmuebleSchema   = new Schema({
+    name: String,
+    codigo: String,
+    hub: [hubSchema]
+});
+var unidadResidencialSchema   = new Schema({
+    name: String,
+    codigo: String,
+    inmueble: [inmuebleSchema]
 });
 
-module.exports = mongoose.model('unidadResidencial', unidadResidencial);
+
+module.exports = mongoose.model('unidadResidencial', unidadResidencialSchema);
