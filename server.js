c9b34471ad181ec538148e0f58c802d88ae9df62
srodriguez20 <s.rodriguez20@uniandes.edu.co>
2018-03-22 23:04:29 -0500

    server.js
    se agregaron las funciones rest de unidadResidencial

--- server.js
+++ server.js
@@ -4,58 +4,27 @@
 // =============================================================================
 
 // call the packages we need
-var alarmas     = require('./models/alarmas'); //importar alarmas
+var alarmas     = require('./models/alarmas');
+var unidadResidencial     = require('./models/unidadResidencial');
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');    //importar body-parser
-var mongoose   = require('mongoose');       //importar mongoose
-var url = 'mongodb://localhost:27017/ArquiHub';    //url de la base de datos MongoDB
-var mqtt = require('mqtt')                  // importar mqtt
-var client  = mqtt.connect('mqtt:172.24.42.92:8083') ///// FALTA CONFIGURAR ESTO, IP de donde está alojado el MQTT
+var bodyParser = require('body-parser');
 
+var mongoose   = require('mongoose');
+var url = 'mongodb://localhost:27017/ArquiHub';
 
-//CONEXIÓN A BASE DE DATOS
-// =================================================================================
 mongoose.connect(url, function(err, db) {
   if (err) throw err;
   console.log("Database joined");
 });
-var db = mongoose.connection; // Variable que representa la conexión
-
-
+console.log(mongoose.connection.readyState);
+var db = mongoose.connection;
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
-
-
-// PARTE DE MQTT
-// ==============================================================================
-client.on('connect', function () {   //Cuando se conecte
-  client.subscribe('alarmas')
-  client.publish('alarmas', 'Hello mqtt')
-})
-
-client.on('message', function (topic, message) { //Cuando haya un mensaje
-  // message is Buffer
-  //var alarma = new alarmas();      // create a new instance of the Bear model
-  //alarma.name = message.body.name;
-  //alarma.descripcion = message.body.descripcion;
-  //alarma.date = message.body.date;
-  //alarma.codigo = message.body.codigo; // set the bears name (comes from the request)
-
-  // save the bear and check for errors
-  //alarma.save(function(err) {
-  //    if (err)
-  //        res.send(err);
-
-  //    res.json({ message: 'Alarma created!' });
-  //});
-  console.log(message.toString())
-  client.end()
-})
 
 // ROUTES FOR OUR API
 // =============================================================================
@@ -64,13 +33,13 @@ var router = express.Router();              // get an instance of the express Ro
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log('Something is happening.'); //Siempre que esto aparezca en consola es porque algo entró.
+    console.log('Something is happening.');
     next(); // make sure we go to the next routes and don't stop here
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-  res.send('Hola');
+  console.log(mongoose.connection.readyState);
 });
 
 // more routes for our API will happen here
@@ -78,29 +47,24 @@ router.get('/', function(req, res) {
 // on routes that end in /bears
 // ----------------------------------------------------
 
-// RUTAS PARA ALARMAS
-// ===================================================================
+
 router.route('/alarmas')
 
-    //Get de todas las alarmas
-    .get(function(req, res) {
-        alarmas.find({}, function finded(err, media){
-          if(err){console.log(err)};
+      .get(function(req, res) {
+      console.log('entró 1');
+      alarmas.find({}, function finded(err, media){
+          if(err){
+            console.log('errosrasfjabf')
+          };
           console.log(media);
           res.json({media});
-        });
-    })
+        }); console.log('JUEPUTA');
+      })
 
-    //Post de todas las alarmas
-    //El cuerpo del post debe ser
-    //  "name": String
-    //  "codigo": String
-    //  "descripcion": String
-    //  "date": String
     .post(function(req, res) {
 
         var alarma = new alarmas();      // create a new instance of the Bear model
-        alarma.name = req.body.name;
+        alarma.name = req.body.alarma;
         alarma.descripcion = req.body.descripcion;
         alarma.date = req.body.date;
         alarma.codigo = req.body.codigo; // set the bears name (comes from the request)
@@ -114,10 +78,81 @@ router.route('/alarmas')
         });
 
     });
+// ruta terminada en /unidadResidencial
+// ----------------------------------------------------
+    router.route('/unidadResidencial')
+
+          .get(function(req, res) {
+          console.log('entró 1');
+          unidadResidencial.find({}, function finded(err, media){
+              if(err){
+                console.log('errosrasfjabf')
+              };
+              console.log(media);
+              res.json({media});
+            });
+          console.log('JUEPUTA');
+          })
+        .post(function(req, res) {
+
+            var unidadResidencial = new alarmas();      // create a new instance of the Bear model
+            unidadResidencial.name = req.body.alarma;
+            unidadResidencial.codigo = req.body.codigo; // set the bears name (comes from the request)
+
+            // save the bear and check for errors
+            alarma.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Alarma created!' });
+            });
+
+        });
+// ruta terminada en /unidadResidencial
+// ----------------------------------------------------
+router.route('/unidadResidencial/:unidadResidencial')
+
+    .get(function(req, res) {
+        unidadResidencial.findById(req.params.unidad_id, function finded(err, media){
+            if(err){
+              console.log('errosrasfjabf')
+            };
+            console.log(media);
+            res.json({media});
+          });
+    })
+    .put(function(req, res) {
+
+        // use our bear model to find the bear we want
+        unidadResidencial.findById(req.params.unidad_id, function(err, media) {
+
+            if (err)
+                res.send(err);
+
+            unidadResidencial.name = req.body.name;  // update the bears info
+
+            // save the bear
+            unidadResidencial.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'unidadResidencial updated!' });
+            });
+
+        });
+    })
+    .delete(function(req, res) {
+        unidadResidencial.remove({
+            _id: req.params.unidad_id
+        }, function(err, media) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
 
 
-// RUTAS PARA UNIDAD RESIDENCIAL
-// ============================================================
 
 
 // REGISTER OUR ROUTES -------------------------------
